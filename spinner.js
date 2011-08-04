4b1167c870538c13145a423230653b4c9524e7ef
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2011-08-04 17:29:04 +0200

    canvas spinner

--- spinner.js
+++ spinner.js
@@ -1,11 +1,22 @@
 Spinner = {
     create : function() {
         var spinner = document.createElement('div');
-        spinner.innerHTML = '<svg class="spinner" xmlns="http://www.w3.org/2000/svg" width="50" height="50"><circle cx="25" cy="25" r="20" fill="none" stroke="black" stroke-width="3" stroke-dasharray="24 18"/></svg>';
         spinner.className = 'bringIn';
         spinner.style.position = 'absolute';
         spinner.style.left = (window.innerWidth-50)/2 + 'px';
         spinner.style.top = (window.innerHeight-50)/2 + 'px';
+        var canvas = document.createElement('canvas');
+        canvas.className = 'spinner';
+        canvas.width = canvas.height = 50;
+        var ctx = canvas.getContext('2d');
+        ctx.lineWidth = 4;
+        var third = Math.PI*2 / 3;
+        for (var i=0; i<3; i++) {
+          ctx.beginPath();
+          ctx.arc(25, 25, 20, i*third, i*third+Math.PI/2, false);
+          ctx.stroke();
+        }
+        spinner.appendChild(canvas);
         return spinner;
     },
     
